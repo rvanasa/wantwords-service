7abe84ec6a11aaa856068729b728c69858b6f616
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-03-07 18:54:34 -0700

    Deploy

---
+++ client/build/precache-manifest.870ee6fcda2d55c2ff31d59ddc66ac4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5df161fca29ba60f044ffea90e741fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7058814a043024647b70",
+    "url": "/static/css/2.d16f4741.chunk.css"
+  },
+  {
+    "revision": "ab427e6b083c3b5bc581",
+    "url": "/static/css/main.4fa7c274.chunk.css"
+  },
+  {
+    "revision": "7058814a043024647b70",
+    "url": "/static/js/2.cc1c93c1.chunk.js"
+  },
+  {
+    "revision": "2d633a2fa4f20f79868d8bc55c98c7af",
+    "url": "/static/js/2.cc1c93c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab427e6b083c3b5bc581",
+    "url": "/static/js/main.8e0227e5.chunk.js"
+  },
+  {
+    "revision": "9fba6d35c46f13fc0658",
+    "url": "/static/js/runtime-main.be098589.js"
+  }
+]);
